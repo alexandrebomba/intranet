84bfe22b499f0f779b4c89b1547b437304c6149f
alexandre <alexandre@localhost.localdomain>
2017-01-22 23:52:30 -0200

    Comentário

--- checkLogin.php
+++ checkLogin.php
@@ -5,4 +5,4 @@ if(!isset($_SESSION['logado'])){
     header('location:login.php');
 }
 
-//Checa se está logado, caso não esteja vai direcionar para login.php
+//Checa se está logado, caso não esteja vai direcionar para login.php...
